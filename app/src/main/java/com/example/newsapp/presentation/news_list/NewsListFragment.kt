c2f81bdb30dec741fb433ff7b30e091e8cccd0b8
Jiban Senapati <jibans@mindfiresolutions.com>
2022-12-02 13:14:09 +0530

    Initial Commit

--- app/src/main/java/com/example/newsapp/presentation/news_list/NewsListFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/news_list/NewsListFragment.kt
@@ -1,9 +1,11 @@
 package com.example.newsapp.presentation.news_list
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.coroutineScope
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.FragmentNewsListBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class NewsListFragment : Fragment() {
@@ -47,22 +50,22 @@ class NewsListFragment : Fragment() {
             )
         )
 
-       /* val pDialog = SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE)
-        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"))
-        pDialog.setTitleText("Loading")
-        pDialog.setCancelable(false)
-        pDialog.show()*/
 
         newsListViewModel.getNewsData()
 
         lifecycle.coroutineScope.launchWhenCreated {
             newsListViewModel.newsList.collect {
+                if (it.isLoading) {
+                    _binding!!.progressBar.display
+                }
+
                 if (it.data != null) {
                     val newsHeadingAdapter = NewsHeadingAdapter(it.data)
                     val newsVerticalAdapter = NewsVerticalListAdapter(it.data)
                     _binding!!.horizontalRecycler.adapter = newsHeadingAdapter
                     _binding!!.verticalRecycler.adapter = newsVerticalAdapter
                 }
+                _binding!!.progressBar.visibility= View.GONE
             }
         }
     }
