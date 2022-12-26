f95983758d1e8e8100f1819eba1ce56b8cc291aa
Jiban Senapati <jibans@mindfiresolutions.com>
2022-12-26 18:29:21 +0530

    Initial Commit

--- app/src/main/java/com/example/newsapp/common/Constants.kt
+++ app/src/main/java/com/example/newsapp/common/Constants.kt
@@ -3,5 +3,4 @@ package com.example.newsapp.common
 object Constants {
 
     const val BASE_URL = "https://newsapi.org/v2/"
-
 }
--- app/src/main/java/com/example/newsapp/presentation/news_list/NewsListFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/news_list/NewsListFragment.kt
@@ -20,7 +20,7 @@ import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
- * Fragment class to show news vertically and horizontally
+ * Fragment class to show news vertically
  */
 @AndroidEntryPoint
 class NewsListFragment : Fragment(), NewsVerticalListAdapter.NewsClickListener {
