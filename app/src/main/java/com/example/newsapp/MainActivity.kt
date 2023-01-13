523c38fea2403f321857977df783d016eda0535c
Jiban Senapati <jibans@mindfiresolutions.com>
2023-01-13 11:40:13 +0530

    Initial Commit

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,11 +2,7 @@ package com.example.newsapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.example.newsapp.presentation.news_bookmark.NewsBookmarkListFragment
